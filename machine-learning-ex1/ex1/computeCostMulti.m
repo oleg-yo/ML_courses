7f336a1c1ba116a19fd47ae5ccc095e7d4565500
oleg <olegyakovchuk@gmail.com>
2018-01-08 00:58:18 +0200

    Add Week 2 exersices full realization

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,11 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
 H = X * theta;
-
 J = sum((H - y).^2) / (2 * m);
-
 
 % =========================================================================
 
